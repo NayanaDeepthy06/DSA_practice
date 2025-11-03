0794165e5ec8c152b6a877bd2cd21141d26c2f1c
Nayana Deepthy <nayanadeepthyr@gmail.com>
2025-11-03 17:38:56 +0530

    Refactor longest subarray with sum k algorithm
    Refactored longest subarray calculation to use a hash map for all integers, replacing the previous approach that only handled non-negative numbers.

--- longest_subarray_withsum_k.java
+++ longest_subarray_withsum_k.java
@@ -1,23 +1,43 @@
 class longest_subarray_withsum_k {
     public int longestSubarray(int[] nums, int k) {
-       int n = nums.length;
-       int left = 0;
-       int right = 0;
-       int sum = nums[0];
-       int maxLength = 0;
-       while(right<n){
-         while(left<=right &&sum > k){
-            sum-=nums[left];
-            left++;
-         }
-         if(sum==k){
-            maxLength = Math.max(maxLength,right-left+1);
-         }
-         right++;
-         if(right<n){
-         sum+= nums[right];
-         }
-       }
-       return maxLength;
+       // int n = nums.length;
+       // int left = 0;
+       // int right = 0;
+       // int sum = nums[0];
+       // int maxLength = 0;
+       // while(right<n){
+       //   while(left<=right &&sum > k){
+       //      sum-=nums[left];
+       //      left++;
+       //   }
+       //   if(sum==k){
+       //      maxLength = Math.max(maxLength,right-left+1);
+       //   }
+       //   right++;
+       //   if(right<n){
+       //   sum+= nums[right];
+       //   }
+       // }
+       // return maxLength; // this is first approach for only +ves and 0's
+    // second approach using hash map works for all numbers 
+    int n =  nums.length;
+    long sum =0;
+    int maxLen = 0;
+    Map<Long,Integer> presum = new HashMap<>();
+    for(int i = 0;i<n;i++){
+        sum+=nums[i];
+     if(sum == k){
+        maxLen = i+1;
+     }
+     long rem = sum - k;
+     if(presum.containsKey(rem)){
+        int len = i - presum.get(rem);
+        maxLen = Math.max(maxLen,len);
+     }
+     if(!presum.containsKey(sum)){
+        presum.put(sum,i);
+     }
+    }
+    return maxLen;
     }
 }
