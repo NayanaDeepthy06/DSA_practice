ff4a4e36cf11ef4277bddb311d9c0193cbc9ccf6
Nayana Deepthy R <nayanadeepthyr@gmail.com>
2026-01-31 16:44:36 +0530

    i have added new row array and col array
    based on row array and col array i have set zeroes in original matrix tie complexity is reduce to n^2

--- setmatrix.java
+++ setmatrix.java
@@ -34,4 +34,27 @@ class setmatrix {
     //     }
     //     }
     // } method 1 of time complexity O(m*n)*(m+n)*m*n and space complexity O(1)
+    // method 2 
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[] row = new int[m];
+        int[] col = new int[n];
+        // m represents rows ans n represents the column 
+        for(int i =0;i<m;i++){
+            for(int j=0;j<n;j++){
+             if(matrix[i][j]==0){
+                row[i] = 1;
+                col[j] = 1;
+             }
+            }
+        }
+        for(int i =0;i<m;i++){
+            for(int j =0;j<n;j++){
+                if(row[i]==1 || col[j]==1){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+    }
 }
