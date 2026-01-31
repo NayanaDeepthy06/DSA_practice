d28a96ba9037de6c81036ee2d2667cede88cdb4c
Nayana Deepthy R <nayanadeepthyr@gmail.com>
2026-01-31 16:42:55 +0530

    Add files via upload

---
+++ setmatrix.java
@@ -0,0 +1,37 @@
+class setmatrix {
+    
+    // public void setZeroes(int[][] matrix) {
+    //     int m = matrix.length;
+    //     int n = matrix[0].length;
+    //     // m represents rows ans n represents the column 
+    //     for(int i=0;i<m;i++){
+    //         for(int j=0;j<n;j++){
+    //             if(matrix[i][j]==0){
+    //                 markRow(i,matrix,m,n);
+    //                 markCol(j,matrix,m,n);
+    //             }
+    //         }
+    //     }
+    //     for(int i=0;i<m;i++){
+    //     for(int j=0;j<n;j++){
+    //         if(matrix[i][j]=='a'){
+    //             matrix[i][j]=0;
+    //         }
+    //     }
+    //   }
+    // }
+    // void markRow(int i,int[][] matrix,int m,int n){
+    //     for(int j=0;j<n;j++){
+    //         if(matrix[i][j]!=0){
+    //         matrix[i][j]='a';
+    //     }
+    //    }
+    // }
+    // void markCol(int j,int[][] matrix,int m,int n){
+    //     for(int i=0;i<m;i++){
+    //         if(matrix[i][j]!=0){
+    //         matrix[i][j]='a';
+    //     }
+    //     }
+    // } method 1 of time complexity O(m*n)*(m+n)*m*n and space complexity O(1)
+}
