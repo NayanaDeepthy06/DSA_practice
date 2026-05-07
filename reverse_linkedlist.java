42bd182d7271da056b15194811406b82143101d8
Nayana Deepthy R <nayanadeepthyr@gmail.com>
2026-05-07 23:27:12 +0530

    this reverse of linked list is done using recursion
    t.c is O(n)
    s.c is O(n) due to n recursive calls are saved in stack memory

---
+++ reverse_linkedlist.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        // if(head==null || head.next ==null){
+        //     return head;
+        // }
+        // ListNode temp = head;
+        // ListNode prev = null;
+        // while(temp!=null){
+        //     ListNode front = temp.next;
+        //     temp.next = prev;
+        //     prev = temp;
+        //     temp = front;
+        // }
+        // return prev; // becoz now the previous is the new head after reversing // the linked list
+        if(head==null || head.next ==null){
+            return head;
+        }
+        ListNode newhead = reverseList(head.next);
+        ListNode front = head.next;
+        front.next = head;
+        head.next = null;
+        return newhead; // this is using recursive method
